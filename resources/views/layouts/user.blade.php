2bc8ed2a1e0f185d1d2314d36f152d5e641c9a0f
Cok Agung Yudi <mamorutokoro@gmail.com>
2017-04-26 12:47:01 +0800

    hem

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -91,12 +91,30 @@
             </a>
             <div class="collapse navbar-collapse" id="navbarNav1">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#about">Daftar</a>
-                    </li>
+                    @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->nama_user }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
                 </ul>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -10,23 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-<<<<<<< HEAD
 /*
 Route::get('/', function () {
 =======
 
 /*Route::get('/', function () {
->>>>>>> 6a74895de58ac2ae26c11e406b1531fc925eea9f
     return view('welcome');
 });*/
 
 Auth::routes();
 
 Route::get('/', 'HomeController@index');
-<<<<<<< HEAD
 Route::get('/home', 'HomeController@index');
-=======
->>>>>>> 6a74895de58ac2ae26c11e406b1531fc925eea9f
 Route::get('/cari', 'HomeController@cari');
 Route::get('/catering', 'CateringController@catering');
 Route::get('/menu', 'CateringController@menu');
