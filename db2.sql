80858e04ac87bee53d906ba8c4f24e08da5e3e47
Cok Agung Yudi <mamorutokoro@gmail.com>
2017-04-28 00:53:48 +0800

    reset password

--- db2.sql
+++ db2.sql
@@ -62,6 +62,31 @@ CREATE TABLE `menu` (
 
 /*Data for the table `menu` */
 
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `migrations` */
+
+/*Table structure for table `password_resets` */
+
+DROP TABLE IF EXISTS `password_resets`;
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `password_resets` */
+
 /*Table structure for table `transaksi` */
 
 DROP TABLE IF EXISTS `transaksi`;
@@ -121,11 +146,11 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT NULL,
   `remember_token` varchar(65) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 /*Data for the table `users` */
 
-insert  into `users`(`id`,`nama_user`,`email`,`password`,`no_telp`,`status_catering`,`nama_catering`,`deskripsi`,`foto_catering`,`no_telp_catering`,`alamat_catering`,`status_user`,`updated_at`,`created_at`,`remember_token`) values (5,'Cok Yudi','cokagungyudi@gmail.com','$2y$10$DElbAYtBFUDn0rg6KZo9Z.3tcVVetygEFI5ZcHcTLoRxE/83dZ3Oe','081238738893',0,NULL,NULL,NULL,NULL,NULL,1,'2017-04-24 18:35:48','2017-04-24 18:35:48','qvpwdyXTziBRtdamSddU4eoKAVkU90Hnb3Zy0dxowqkpcycbFfBUQPYD2RSB');
+insert  into `users`(`id`,`nama_user`,`email`,`password`,`no_telp`,`status_catering`,`nama_catering`,`deskripsi`,`foto_catering`,`no_telp_catering`,`alamat_catering`,`status_user`,`updated_at`,`created_at`,`remember_token`) values (5,'Cok Yudi','cokagungyudi@gmail.com','$2y$10$jJ/pdnBcIAhbvZAXiGlQ2ePNy/Ht0RKnNF7ltTJ87h2lFYP7t332y','081238738893',1,'Nasjing Aget','balblablablablablblalablaabla',NULL,NULL,'Jimbaran',1,'2017-04-27 16:49:56','2017-04-24 18:35:48','rQZCsDAMMhDgr8MQuaJGkuqFFuJtWGISRihooEeKYTUsUkUDnfIrwYD1kDQW'),(6,'Ecek','ecek@gmail.com','$2y$10$66o.8Xe0RZJfLsfi2dwh1.A19K2u7C9gZed5Sw.LiP.y62kALV3Sy','089988998889',1,'UBK','wkwkwkwkwkkwwkwkwkkwwk',NULL,NULL,'Kampus Unud',1,'2017-04-26 16:40:04','2017-04-26 16:40:04','r6ZVfURLnAONQTXf0gXqSCkJhFN7cKBAZZsUbe5fFeq9UF7vRne0BqpfoUIX'),(7,'wira','wira@gmail.com','$2y$10$w28E6RgAzSc3cp0YZHWAAemFE/vICGiGDSlzLMeFJYBry13/.gY5G','0817267736728',1,'Mek Pon','nasi campur isi telor dadar',NULL,NULL,'Kampus Ilkom',1,'2017-04-26 16:42:21','2017-04-26 16:42:21','SbWSWyuRHmrvRie8wbqHSM2bH4NBCZZUNkPW5d7jmlaDmrWgm2o5vnmpA2C1'),(8,'kim jong un','kimikimi@gmail.com','$2y$10$.Fzz1IaMKPKuqPmTYSag7.KqaDRRcVZLrxH7jW83PfjJRRrf8wTVO','0812839829',1,'Mad Dog','nasi kuning sing misi tempe',NULL,NULL,'Goa Gong',1,'2017-04-26 16:46:32','2017-04-26 16:46:32','1ZvZLHAXNOZPoMhTIp5mohjCGa4MstmwC096LlD8pMbYorwR7e5akHDYZlN3'),(9,'kicir','kicir@gmail.com','$2y$10$DFvA3TsfYD1G9t.FJraAeOpYjNRtOCntBHLcr6VhJmzfjyMlU6Zbe','087788999888',0,NULL,NULL,NULL,NULL,NULL,1,'2017-04-26 19:01:42','2017-04-26 19:01:42',NULL),(10,'chicharito','cekcek@gmail.com','$2y$10$P0CiftJ5Y/WJ5Hp6PV17Cu/RnirbrJhOtq6xlpZYSxawID6VgPsC2','123123123',0,NULL,NULL,NULL,NULL,NULL,1,'2017-04-27 16:16:32','2017-04-27 16:16:32','mGq6MV0ozH1vmKcBSbz3h1qRj1EGFTEtXGxqIZEQVpCQNysOmkIG5Olkqbpd');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
