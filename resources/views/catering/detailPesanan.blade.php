e1c80b9146cf44c6898121513e2bd115a974ecec
Cok Agung Yudi <mamorutokoro@gmail.com>
2017-05-31 12:50:04 +0800

    detail

--- resources/views/catering/detailPesanan.blade.php
+++ resources/views/catering/detailPesanan.blade.php
@@ -1,7 +1,7 @@
 @foreach($detail as $a)
-<p>
-    <span id="namaItem">{{$a->nama_item}}</span>-
+<ol>
+    <span id="namaItem">{{$a->nama_item}}</span> -
     <span id="qtyItem">{{$a->qty_item}}</span>
     <span id="satuanItem">{{$a->satuan_item}}</span>
-</p>
+</ol>
 @endforeach
